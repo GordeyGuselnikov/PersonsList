869cec0ef324768adf5d125fb056694de62e7181
Гордей Гусельников <guselnikovgordey@icloud.com>
2020-06-10 14:53:12 +0700

    Initial Commit

---
+++ PersonsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonsList
+//
+//  Created by Гордей Гусельников on 10.06.2020.
+//  Copyright © 2020 gordeyStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
